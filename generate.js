79cf4b6555037e5eb37298c7c9e5c92dc1f4e1de
Fidanze <64784660+Fidanze@users.noreply.github.com>
2023-11-28 01:15:56 +0500

    add generate.js

---
+++ generate.js
@@ -0,0 +1,42 @@
+async function parseFunc() {
+  function delay() {
+    return new Promise(resolve => setTimeout(resolve, 1 /*time of delay*/));
+  }
+
+  const table = document.getElementById('tablePassword')
+  const rowCount = table.childElementCount
+
+  let data = new Array()
+
+  for (let i = 0; i < rowCount; i++) {
+    const row = table.children[i]
+    const website = row.children[1].textContent
+    const login = row.children[2].textContent
+
+    const passwordInputBeforeShowing = row.children[3].children[0]
+    passwordInputBeforeShowing.click()
+    await delay()
+    const passwordEye = row.children[3].children[1]
+    passwordEye.click()
+    await delay()
+
+    const password = table.children[i].children[3].children[0].value
+    console.log(`${i + 1}/${rowCount}`)
+    data.push([login, password, website])
+  }
+
+  data.unshift(['Username', 'Password', 'URL'])
+  const csvContent = data.map(e => e.join(",")).join("\n");
+
+  // Create a blob
+  let blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+  let url = URL.createObjectURL(blob);
+
+  // Create a link to download it
+  let pom = document.createElement('a');
+  pom.href = url;
+  pom.setAttribute('download', 'passwords.csv' /*name of file*/);
+  pom.click();
+}
+
+await parseFunc()
